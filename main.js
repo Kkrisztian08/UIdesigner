22b795ccb63fd0020f4cd29ec9e4d84d0f0584ac
Kkrisztian08 <k.krisztian0830@gmail.com>
2021-11-09 16:46:22 +0100

    kész

--- main.js
+++ main.js
@@ -1,53 +1,47 @@
-
-function szovegMegvaltozott(){
-    let szoveg= document.getElementById("szoveg").value;
-    document.getElementById("elonezet").innerHTML = szoveg;
-}
-
-function szovegBillentyu(e){
-    e.stopPropagation();
-}
-
-function meretMegvaltozott(){
-    let meret= document.getElementById("meret").value;
-    if(meret<=0){
-        document.getElementById("meretHiba").innerHTML = "A méret nem lehet 0 vagy kevesebb!";
-    }else if(meret===!isNaN){
-        document.getElementById("meretHiba").innerHTML = "A méretet számmal kell megadni!";
-    }else{
-        document.getElementById("meretHiba").innerHTML = "";
+function szovegbeallitas() {
+    let szoveg = document.getElementById("mintaszoveg").value;
+    if (szoveg.trim() === "") {
+        document.getElementById("szoveghiba").innerHTML = "A mintaszöveg nem lehet üres!";
+        document.getElementById("elonezet").innerHTML = "";
     }
-    document.getElementById("elonezet").style.fontSize = meret+"pt";
+    else{
+        document.getElementById("szoveghiba").innerHTML = "";
+        document.getElementById("elonezet").innerHTML = szoveg;
+    }
 }
 
-
-
-function meretBillentyu(e){
-
-    e.stopPropagation();
-
+function betumeretbeallitas() {
+    let meret = document.getElementById("betumeret").value;
+    document.getElementById("elonezet").style.fontSize = meret + "pt";
 }
 
-function meretBillentyu(e){
-    e.stopPropagation();
-}
-function szovegMegvaltozott(){
-    let meret= document.getElementById("meret").value;
-    document.getElementById("elonezet").style.fontSize = meret+"pt";
+function betuszinbeallitas() {
+    let betuszin = document.getElementById("betuszin").value;
+    document.getElementById("elonezet").style.color = betuszin;
 }
 
+function hatterbeallitas() {
+    let hetterszin = document.getElementById("hatterszin").value;
+    document.getElementById("elonezet").style.backgroundColor = hetterszin;
+}
 
+function visszaallit() {
+    document.getElementById("elonezet").removeAttribute("style");
+    document.getElementById("mintaszoveg").value = "Árvíztűrő tükörfúrógép";
+    document.getElementById("betumeret").value = "12";
+    document.getElementById("betuszin").value = "#000000";
+    document.getElementById("hatterszin").value = "#FFFFFF";
+    document.getElementById("elonezet").innerHTML = "Árvíztűrő tükörfúrógép";
+    document.getElementById("szoveghiba").innerHTML = ""
 
-function szovegBillentyu(e){
-    e.stopPropagation();
 }
 
 function init() {
-    document.getElementById("szoveg").addEventListener("input", szovegMegvaltozott);
-    document.addEventListener("keydown", szovegBillentyu);
-
-    document.getElementById("meret").addEventListener("input", meretMegvaltozott);
-    document.addEventListener("keydown", meretBillentyu);
-
+    document.getElementById("mintaszoveg").addEventListener("input", szovegbeallitas);
+    document.getElementById("betumeret").addEventListener("input", betumeretbeallitas);
+    document.getElementById("betuszin").addEventListener("input", betuszinbeallitas);
+    document.getElementById("hatterszin").addEventListener("input", hatterbeallitas);
+    document.getElementById("alap").addEventListener("click", visszaallit);
 }
+
 document.addEventListener("DOMContentLoaded", init);
